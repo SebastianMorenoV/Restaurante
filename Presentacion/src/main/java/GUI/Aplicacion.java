04bcff9bf99b80f84dc29abfc31f35bdddbaf06f
SebastianMoreno <erick.moreno252840@potros.itson.edu.mx>
2025-03-30 03:16:50 -0700

    Initial commit

---
+++ Presentacion/src/main/java/GUI/Aplicacion.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package GUI;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author Sebastian Moreno
+ */
+public class Aplicacion {
+
+    private JFrame framePrincipal; // Ventana principal
+    MenuPrincipalRestaurante menuPrincipal;
+
+    public Aplicacion() {
+        framePrincipal = new JFrame("Sistema Carnicería");
+        framePrincipal.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        framePrincipal.setSize(1150, 700);
+        framePrincipal.setLocationRelativeTo(null); // Centrar pantalla
+
+        //inicializar pantallas
+        menuPrincipal = new MenuPrincipalRestaurante();
+    }
+
+    public void mostrarMenuPrincipal() {
+        cambiarPantalla(menuPrincipal);
+    }
+
+    // Cambiar de pantalla dentro del frame principal
+    private void cambiarPantalla(JPanel nuevaPantalla) {
+        framePrincipal.getContentPane().removeAll(); // Eliminar contenido anterior
+        framePrincipal.getContentPane().add(nuevaPantalla);
+        framePrincipal.revalidate();
+        framePrincipal.repaint();
+        framePrincipal.setVisible(true);
+    }
+
+}
---
+++ Presentacion/src/main/java/com/carniceria/presentacion/Main.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package com.carniceria.presentacion;
+
+import GUI.Aplicacion;
+
+/**
+ *
+ * @author Sebastian Moreno
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Aplicacion app = new Aplicacion();
+        app.mostrarMenuPrincipal();
+    }
+    
+}
