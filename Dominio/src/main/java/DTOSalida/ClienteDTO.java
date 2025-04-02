90a739c5b65d0fa458938c874c80e65771653226
SebastianMoreno <erick.moreno252840@potros.itson.edu.mx>
2025-04-01 23:26:29 -0700

    BO cliente
    Empece el metodo para guardar un cliente

--- Dominio/src/main/java/DTOSalida/ClienteDTO.java
+++ Dominio/src/main/java/DTOSalida/ClienteDTO.java
@@ -15,4 +15,66 @@ public class ClienteDTO {
     private int visitasTotales;
     private double totalGastado;
     private int puntos;
+
+    public ClienteDTO() {
+    }
+
+    public ClienteDTO(String nombreCompleto, String correo, String telefono, int visitasTotales, double totalGastado, int puntos) {
+        this.nombreCompleto = nombreCompleto;
+        this.correo = correo;
+        this.telefono = telefono;
+        this.visitasTotales = visitasTotales;
+        this.totalGastado = totalGastado;
+        this.puntos = puntos;
+    }
+
+    public String getNombreCompleto() {
+        return nombreCompleto;
+    }
+
+    public void setNombreCompleto(String nombreCompleto) {
+        this.nombreCompleto = nombreCompleto;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public int getVisitasTotales() {
+        return visitasTotales;
+    }
+
+    public void setVisitasTotales(int visitasTotales) {
+        this.visitasTotales = visitasTotales;
+    }
+
+    public double getTotalGastado() {
+        return totalGastado;
+    }
+
+    public void setTotalGastado(double totalGastado) {
+        this.totalGastado = totalGastado;
+    }
+
+    public int getPuntos() {
+        return puntos;
+    }
+
+    public void setPuntos(int puntos) {
+        this.puntos = puntos;
+    }
+    
+    
 }
--- Negocio/src/main/java/BO/ClienteBO.java
+++ Negocio/src/main/java/BO/ClienteBO.java
@@ -6,16 +6,22 @@ package BO;
 
 import DTOEntrada.CrearClienteDTO;
 import DTOSalida.ClienteDTO;
+import Entidades.ClienteFrecuente;
 import exception.NegocioException;
+import exception.PersistenciaException;
 import interfaces.IClienteBO;
 import interfaces.IClienteDAO;
+import java.time.LocalDate;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
  * @author Sebastian Moreno
  */
-public class ClienteBO implements IClienteBO{
-     private IClienteDAO clienteDAO;
+public class ClienteBO implements IClienteBO {
+
+    private IClienteDAO clienteDAO;
 
     public ClienteBO(IClienteDAO clienteDAO) {
         this.clienteDAO = clienteDAO;
@@ -23,8 +29,34 @@ public class ClienteBO implements IClienteBO{
 
     @Override
     public ClienteDTO registrarCliente(CrearClienteDTO clienteDTO) throws NegocioException {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+
+        if (clienteDTO.getNombres().isEmpty()) {
+            throw new NegocioException("El nombre no debe de estar vacio!.");
+        }
+        if (clienteDTO.getApellidoPaterno().isEmpty()) {
+            throw new NegocioException("Se necesita un apellido paterno!.");
+        }
+        if (clienteDTO.getTelefono().isEmpty()) {
+            throw new NegocioException("Se necesita un telefono!.");
+        }
+
+        String cadenaNombre;
+        if (clienteDTO.getApellidoMaterno().isEmpty()) {
+            cadenaNombre = clienteDTO.getNombres() + "" + clienteDTO.getApellidoPaterno();
+        } else {
+            cadenaNombre = clienteDTO.getNombres() + "" + clienteDTO.getApellidoPaterno() + "" + clienteDTO.getApellidoMaterno();
+        }
+
+        ClienteFrecuente cliente = new ClienteFrecuente(0, 0, 0, cadenaNombre, clienteDTO.getTelefono(), clienteDTO.getCorreoElectronico(), LocalDate.now());
+
+        try {
+            clienteDAO.guardarClienteFrecuente(cliente);
+            return new ClienteDTO();
+        } catch (PersistenciaException ex) {
+            Logger.getLogger(ClienteBO.class.getName()).log(Level.SEVERE, null, ex);
+            throw new NegocioException("Error al guardar el cliente." + ex.getMessage(), ex);
+        }
+
     }
-     
-     
+
 }
